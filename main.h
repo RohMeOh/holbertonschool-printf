96e22f11c47b9f105b0be5f50f0e8cd2b625139a
RohMeOh <romeogr21@gmail.com>
2026-03-18 21:03:47 -0400

    Correct comment formatting and grammatical correction

--- main.h
+++ main.h
@@ -4,32 +4,30 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* BRAIN (CONTROLLER)
- * int - returns a number.
- * _printf - function name.
- * char * - points to the first character of a string.
- * const - shields string from being modified
- * format - the string read by the function.
- * ... means function can receive multiple arguments of different types 
- */
+// BRAIN (CONTROLLER)
+// int - returns a number.
+// _printf - function name.
+// char * - points to the first character of a string.
+// const - shields string from being modified.
+// format - the string read by the function.
+// ... means function can receive multiple arguments of different types.
 int _printf(const char *format, ...);
 
-/* NERVOUS SYSTEM (HANDLER)
- * int - returns number of character printed.
- * handle_specifier - decision maker.
- * char c - specifier.
- * va_list args - list of extra arguments.
- */
+// NERVOUS SYSTEM (HANDLER)
+// int - returns number of characters printed.
+// handle_specifier - decision maker.
+// char c - specifier.
+// va_list args - list of extra arguments.
 int handle_specifier(char c, va_list args);
 
-/* MUSCULAR SYSTEM (HELPERS)
- * int - returns count.
- * print_string - function name.
- * char *s - points to the first character of the string.
- * print_char - function name.
- * char - stores a single character value. 
- */
+// MUSCULAR SYSTEM (HELPERS)
+// int - returns count.
+// print_string - function name.
+// char *s - points to the first character of the string.
+// print_char - function name.
+// char - stores a single character value.
 int print_string(char *s);
-int print_char(char);
+int print_char(char c);
 
 #endif
+
